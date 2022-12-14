6b40fe8d7a0ed1900fde191b97d91cb6ed60ac33
Nevilkumar <nevil.0619@gmail.com>
2022-12-14 17:25:08 +0530

    Responsive Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
+
+  const [mobileView, setMobileView] = useState(false);
+
+  useEffect(() => {
+    setMobileView(false);
+  }, []);
+
   return (
     <div className="app">
-      <Navbar />
-      <Content />
-      <Footer />
+      <Navbar mobileView = {mobileView} setMobileView = {setMobileView} />
+      {
+        !mobileView &&
+        <>
+          <Content />
+          <Footer />
+        </>
+      }
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,30 @@
 import React from 'react'
-
 import './Navbar.css'
 
-const Navbar = () => {
+const Navbar = ({mobileView, setMobileView }) => {
+
   return (
     <div className='nav-container'>
-        <div className='nav-heading-container'>
-            <a href='#' className='nav-logo'></a>
-        </div>
-        {/* <div>Menu</div> */}
-        <div className='nav-links-container--desktop'>
-            <a href='#' className='nav-links'>Features</a>
-            <a href='#' className='nav-links'>Pricing</a>
-            <a href='#' className='nav-links highlighted-link'>Help</a>
-            <a href='#' className='nav-links'>Sign in</a>
-            <a href='#' className='nav-links nav-links--blue'>Create a Nolt board</a>
-        </div>
+      <div className='nav-heading-container'>
+        <a href='#' className='nav-logo'></a>
+        <div className={mobileView ? 'nav-links highlighted-link nav-menu' : 'nav-links nav-menu'} onClick={() => setMobileView((prev) => !prev)}>Menu</div>
+      </div>
+      <div className={mobileView ? "nav-links-container--mobile mobile-active" : "nav-links-container--mobile"}>
+        <a href='#' className='nav-links'>Sign in</a>
+        <a href='#' className='nav-links'>Create a Nolt board</a>
+        <div className='nav-links__partition-line'></div>
+        <a href='#' className='nav-links'>Features</a>
+        <a href='#' className='nav-links'>Pricing</a>
+        <a href='#' className='nav-links'>Help</a>
+      </div>
+
+      <div className='nav-links-container--desktop'>
+        <a href='#' className='nav-links'>Features</a>
+        <a href='#' className='nav-links'>Pricing</a>
+        <a href='#' className='nav-links highlighted-link'>Help</a>
+        <a href='#' className='nav-links'>Sign in</a>
+        <a href='#' className='nav-links nav-links--blue'>Create a Nolt board</a>
+      </div>
     </div>
   )
 }
